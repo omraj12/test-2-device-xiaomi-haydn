e386dfb2eac444e5e4efa2d3157335148c4bfe13
ugly-kid-af <dwarmachine24@gmail.com>
2022-06-11 13:46:00 +0600

    haydn: Bring-up Elixir
    Signed-off-by: “omraj12” <“omdoulagar12345@gmail.com”>

--- aosp_haydn.mk
+++ aosp_haydn.mk
@@ -11,11 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from haydn device
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common Project Elixir stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+FORCE_LAWNCHAIR := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_haydn
