1ea8ee25cacde893d4b8abf673d663ea20fdd340
omraj12 <rajesh.doulagar8@gmail.com>
2022-07-09 11:44:34 +0530

    haydn: vendorsetup.sh: Clone firmware and update kernel repo
    Signed-off-by: omraj12 <rajesh.doulagar8@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,11 @@
 rm -rf kernel/xiaomi/haydn
-git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_haydn kernel/xiaomi/haydn
+git clone --depth=1 https://github.com/PixelExperience-Devices/kernel_xiaomi_haydn kernel/xiaomi/haydn
 
 rm -rf vendor/xiaomi/haydn
 git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_haydn vendor/xiaomi/haydn
 
 rm -rf vendor/xiaomi/haydn-miuicamera
 git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_haydn-miuicamera -b twelve vendor/xiaomi/haydn-miuicamera
+
+rm -rf vendor/xiaomi/haydn-firmware
+git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_haydn-firmware vendor/xiaomi/haydn-firmware
