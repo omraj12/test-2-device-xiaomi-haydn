7ebd0fda27f39edc33ea136e6a87c0fa7daf688a
Murat Kozan <muratkozan350@gmail.com>
2022-08-31 19:25:32 +0300

    haydn: init: Override odm_dlkm and vendor_dlkm props

--- init/init_haydn.cpp
+++ init/init_haydn.cpp
@@ -39,10 +39,12 @@ std::vector<std::string> ro_props_default_source_order = {
     "",
     "bootimage.",
     "odm.",
+    "odm_dlkm.",
     "product.",
     "system.",
     "system_ext.",
     "vendor.",
+    "vendor_dlkm.",
 };
 
 void property_override(char const prop[], char const value[], bool add = true)
