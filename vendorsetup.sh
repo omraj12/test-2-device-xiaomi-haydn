15db733a1678f4f6f538829f72fd25f6ac37a53e
omraj12 <rajesh.doulagar8@gmail.com>
2022-09-20 08:47:21 +0000

    haydn: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,13 +2,10 @@ rm -rf kernel/xiaomi/haydn
 git clone --depth=1 https://github.com/PixelExperience-Devices/kernel_xiaomi_haydn kernel/xiaomi/haydn
 
 rm -rf vendor/xiaomi/haydn
-git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_haydn vendor/xiaomi/haydn
-
-rm -rf vendor/xiaomi/haydn-miuicamera
-git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_haydn-miuicamera -b twelve vendor/xiaomi/haydn-miuicamera
+git clone --depth=1 https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn.git vendor/xiaomi/haydn
 
 rm -rf vendor/xiaomi/haydn-firmware
 git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_haydn-firmware vendor/xiaomi/haydn-firmware
 
 rm -rf vendor/Gcam
-git clone https://gitlab.com/sanjeevstunner/vendor-g-cam-bsg.git -b main vendor/Gcam
+git clone --depth=1 https://gitlab.com/sanjeevstunner/vendor-g-cam-bsg.git -b main vendor/Gcam
